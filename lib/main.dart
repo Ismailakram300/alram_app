435ac16468113bd1c0861e561479bc0bd7c04813
Ismailakram300 <ismailakram300@gmail.com>
2025-06-02 13:54:14 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -49,15 +49,4 @@ class MyHomePage extends StatefulWidget {
           children: <Widget>[
             const Text('You have pushed the button this many times:'),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-      , // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
+          
