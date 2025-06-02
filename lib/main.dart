19b1e2352eb0ff19d83e2651ee479a8d81859604
Ismailakram300 <ismailakram300@gmail.com>
2025-06-02 13:53:44 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -39,8 +39,7 @@ class MyHomePage extends StatefulWidget {
       appBar: AppBar(
 
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+      
         title: Text(widget.title),
       ),
       body: Center(
