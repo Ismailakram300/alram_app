f95eaecad0582b3bf695047a733dc2469a6a677f
Ismailakram300 <ismailakram300@gmail.com>
2025-06-02 15:53:24 +0500

    update

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_alarm_clock/flutter_alarm_clock.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -16,8 +17,52 @@ class _HomePageState extends State<HomePage> {
         backgroundColor: Colors.cyan,
 
         title: Text('Alarm App'),
-        centerTitle: true
+        centerTitle: true,
+      ),
 
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Container(
+              margin: const EdgeInsets.all(25),
+              child: TextButton(
+                child: const Text(
+                  'Create alarm at 23:59',
+                  style: TextStyle(fontSize: 20),
+                ),
+                onPressed: () {
+                  FlutterAlarmClock.createAlarm(hour: 15, minutes: 51,title: "helo");
+                },
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.all(25),
+              child: const TextButton(
+                onPressed: FlutterAlarmClock.showAlarms,
+                child: Text('Show alarms', style: TextStyle(fontSize: 20)),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.all(25),
+              child: TextButton(
+                child: const Text(
+                  'Create timer for 42 seconds',
+                  style: TextStyle(fontSize: 20),
+                ),
+                onPressed: () {
+                  FlutterAlarmClock.createTimer(length: 42);
+                },
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.all(25),
+              child: const TextButton(
+                onPressed: FlutterAlarmClock.showTimers,
+                child: Text('Show Timers', style: TextStyle(fontSize: 20)),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
